bb8217f1dcc493b8f9b577fbe4ae78bdacbe6eb9
benhadda <fouad.benhadda@gmail.com>
2026-02-05 17:58:17 +0100

    feat : add 404 page

--- src/app/restaurant/[slug]/page.jsx
+++ src/app/restaurant/[slug]/page.jsx
@@ -1,30 +1,26 @@
 "use client";
 
+
 import { use, useState } from "react"; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { notFound } from 'next/navigation';
 import datas from '@/data/restaurants.json';
 import MenuItem from '@/components/MenuItem/MenuItem';
 
 export default function RestaurantPage({ params }) {
   const { slug } = use(params); 
   const restaurant = datas.restaurants.find((resto) => resto.slug === slug);
-
-  if (!restaurant) {
-    return (
-      <div style={{ textAlign: 'center', padding: '4rem' }}>
-        <h1>404</h1>
-        <p>Restaurant non trouvé</p>
-        <a href="/">Retour à l'accueil</a>
-      </div>
-    );
-  }
   
   const [isLiked, setIsLiked] = useState(false);
 
   const handleLike = () => {
     setIsLiked(!isLiked);
   };
+
+    if (!restaurant) {
+    notFound();
+  }
 
   return (
     <div>
